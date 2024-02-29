ac5a0a996a1991aaa8412efc411d31d8892d0714
StephYa7 <1000p@mail.ru>
2024-02-29 23:30:18 +0700

    Initial commit

---
+++ src/main/java/st/gatewayspring/GatewaySpringApplication.java
@@ -0,0 +1,13 @@
+package st.gatewayspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/st/gatewayspring/GatewaySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package st.gatewayspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
